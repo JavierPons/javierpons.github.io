68f1d0c9b7c3e11d2fd76ed2039b1aece89541ff
Javier Pons <jpescandell@hotmail.com>
2019-04-08 21:22:05 +0300

    loop text

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Javier Pons", "Junior Web Developer", "Junior Full Stack"],
       typeSpeed: 100,
       loop: true,
     });
