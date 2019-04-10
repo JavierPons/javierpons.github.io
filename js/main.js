3cfc91ac81e36f64a0421bdf2584a2d020380ddf
Javier Pons <jpescandell@hotmail.com>
2019-04-10 14:08:59 +0300

    change main.js

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Javier Pons", "Junior Web Developer", "Junior Full Stack"],
+      strings: ["Javier Pons", "Junior Web Developer"],
       typeSpeed: 100,
       loop: true,
     });
